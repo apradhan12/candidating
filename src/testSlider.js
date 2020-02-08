d36e78927c1dc4784877e4198c60e16e850b6000
rahuls135 <rahshah135@gmail.com>
2020-02-08 14:29:56 -0500

    latest updated create account screen with extra test slider file

---
+++ src/testSlider.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: 300,
+  },
+  margin: {
+    height: theme.spacing(10000),
+  },
+}));
+
+
+
+function valuetext(value) {
+  return `${value}`;
+}
+
+export default function DiscreteSlider() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Typography id="discrete-slider" gutterBottom>
+        Education
+      </Typography>
+      <Slider
+        defaultValue={0}
+        getAriaValueText={valuetext}
+        aria-labelledby="discrete-slider"
+        valueLabelDisplay="auto"
+        step={1}
+        marks
+        min={0}
+        max={10}
+      />
+    </div>
+  );
+}
