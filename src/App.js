9b33ea0e4c4d62f07924b6eb01fb73662f18349d
Avery Peterson <averypeterson115@gmail.com>
2020-02-08 17:17:10 -0500

    fix routing issue

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import './App.css';
+
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 import Deck from './Deck';
 import Matches from './Matches';
 import React from 'react';
+import SignIn from './SignIn';
 
 function App() {
 	return (
 		<Router>
 			<div>
 				<Switch>
-					<Route path="/">
-						<div className="App">
-							<h1>Sign In</h1>
-							<header className="App-header">
-								<form>
-									<label>
-										Name:
-										<input type="text" name="name" />
-									</label>
-									<input type="submit" value="Submit" />
-								</form>
-							</header>
-						</div>
+					<Route path="/signin">
+						<SignIn />
 					</Route>
 					<Route path="/matching">
 						<Deck />
