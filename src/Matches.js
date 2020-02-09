42f8b5f9cadae181d257036dd52b515696c0484f
Avery Peterson <averypeterson115@gmail.com>
2020-02-09 10:54:16 -0500

    add button

--- src/Matches.js
+++ src/Matches.js
@@ -7,31 +7,32 @@ import data from './data.js';
 class Matches extends React.Component {
 	render() {
 		const tbody = [];
-
-		for (let i = 0; i < this.props.location.state.matches.length; i++) {
-			tbody.push(
-				<tr key={i}>
-					<td>
-						<div>
-							<img
-								className="candidate-icon"
-								alt="candidate"
-								src={data[this.props.location.state.matches[i]].pic}
-							/>
-						</div>
-					</td>
-					<td>
-						<a
-							href={data[this.props.location.state.matches[i]].website}
-							rel="noopener noreferrer"
-							target="_blank"
-						>
-							{data[this.props.location.state.matches[i]].name}
-						</a>
-					</td>
-					<td>{data[this.props.location.state.matches[i]].party}</td>
-				</tr>
-			);
+		if (this.props.location.state) {
+			for (let i = 0; i < this.props.location.state.matches.length; i++) {
+				tbody.push(
+					<tr key={i}>
+						<td>
+							<div>
+								<img
+									className="candidate-icon"
+									alt="candidate"
+									src={data[this.props.location.state.matches[i]].pic}
+								/>
+							</div>
+						</td>
+						<td>
+							<a
+								href={data[this.props.location.state.matches[i]].website}
+								rel="noopener noreferrer"
+								target="_blank"
+							>
+								{data[this.props.location.state.matches[i]].name}
+							</a>
+						</td>
+						<td>{data[this.props.location.state.matches[i]].party}</td>
+					</tr>
+				);
+			}
 		}
 
 		return (
@@ -46,6 +47,11 @@ class Matches extends React.Component {
 						<a href="https://vote.gov/" rel="noopener noreferrer" target="_blank">
 							Register to vote!
 						</a>
+					</div>
+					<div className="reset-wrapper">
+						<Link to="/representatives">
+							<button className="btn btn-primary">Your Current Elected Officials</button>
+						</Link>
 					</div>
 					<div className="reset-wrapper">
 						<Link to="/matching">
