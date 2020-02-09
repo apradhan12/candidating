50feb863265a8fbea1290e79766774137a145302
Akash Shah <akashshah@Akashs-MacBook-Pro-2.local>
2020-02-08 22:53:14 -0500

    changes to card data view

--- src/Card.js
+++ src/Card.js
@@ -4,46 +4,82 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import data from './data';
 
+
 class Card extends React.Component {
+
 	render() {
+
 		const tbody = [];
+		const tbodyopinion = []
 		const { i, x, y, rot, scale, trans, bind, data } = this.props;
 		const { keyIssues, pic, issues } = data[i];
 
 		console.log(issues);
-		for(let i = 0; i<issues.length; i++) {
-			tbody.push(issues[i].name + ":" + issues[i].opinion + "\n")
+		for (let i = 0; i < issues.length; i++) {
+			const tbodyname = issues[i].name
+			const tbodyopinion = issues[i].opinion
+
+			tbody.push(<tr><td>{tbodyname}</td>
+				<td>{tbodyopinion}</td></tr>)
+
+
 		}
+
+
+
+
+
 		return (
 			<animated.div
 				key={i}
 				className="card-wrapper"
 				style={{
-					transform: interpolate([ x, y ], (x, y) => `translate3d(${x}px,0px,0)`)
+					transform: interpolate([x, y], (x, y) => `translate3d(${x}px,0px,0)`)
 				}}
 			>
 				<animated.div
 					{...bind(i)}
 					className="inner-card-wrapper"
 					style={{
-						transform: interpolate([ rot, scale ], trans)
+						transform: interpolate([rot, scale], trans)
 					}}
 				>
 					<div className="candidate-card">
-            <div className="candidate-image-wrapper">
-              <div className="candidate-image rounded">
-                <img src={pic} className="blur" alt="profilePicture" />
-              </div>
-            </div>
+						<div className="candidate-image-wrapper">
+							<div className="candidate-image rounded">
+								<img src={pic} className="blur" alt="profilePicture" />
+							</div>
+						</div>
 						<br />
 						<div>
 							<span className="key-issues">
 								<b>Key Issues:</b> {keyIssues}
 							</span>
-							{tbody}
-								
-							
-							
+							<br />
+
+							<div class="container">
+								<div class="table-responsive">
+									<table class="table table-sm card-table">
+										<thead>
+											<tr>
+												<th>Issue</th>
+
+												<th>Opinion</th>
+
+											</tr>
+
+										</thead>
+										<tbody>
+											{tbody}
+										</tbody>
+									</table>
+
+								</div>
+							</div>
+
+
+
+
 							<span />
 						</div>
 					</div>
