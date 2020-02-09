addd3e2951afea81c938c97e74ce2bf71d739df3
Avery Peterson <averypeterson115@gmail.com>
2020-02-08 20:37:12 -0500

    add nav bar

--- src/App.js
+++ src/App.js
@@ -1,47 +1,47 @@
 import './App.css';
 
-import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 import Deck from './Deck';
 import Matches from './Matches';
 import React from 'react';
 
-
 function App() {
 	return (
-	  <Router>
-		<div>
-		  <Switch>
-			<Route exact path="/">
-			  <div className="App">
-				  <h1><b>Sign In</b></h1>
-				  <header className="App-header">
-					<form>
-					  <label>
-						Name:
-						<input type="text" name="name" />
-					  </label>
-					  <p/>
-					  <label>
-						ZIP Code:
-						<input type="text" name="name" />
-					  </label>
-					  <p/>
-					  <button className = "App-button">
-						<Link to="/matching">
-						  <button className="btn btn-primary">Create Account</button>
-						</Link>
-					  </button>			
-					</form>
-				</header>
-			  </div>
-			</Route>
-			  <Route path="/matching">
-				<Deck />
-			  </Route>
-			<Route path="/matches" component={Matches} />
-		  </Switch>
-		</div>
+		<Router>
+			<nav className="navbar navbar-light">Candidating</nav>
+			<Switch>
+				<Route exact path="/">
+					<div className="App">
+						<h1>
+							<b>Sign In</b>
+						</h1>
+						<header className="App-header">
+							<form>
+								<label>
+									Name:
+									<input type="text" name="name" />
+								</label>
+								<p />
+								<label>
+									ZIP Code:
+									<input type="text" name="name" />
+								</label>
+								<p />
+								<button className="App-button">
+									<Link to="/matching">
+										<button className="btn btn-primary">Create Account</button>
+									</Link>
+								</button>
+							</form>
+						</header>
+					</div>
+				</Route>
+				<Route path="/matching">
+					<Deck />
+				</Route>
+				<Route path="/matches" component={Matches} />
+			</Switch>
 		</Router>
 	);
 }
